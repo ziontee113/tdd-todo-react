ab745ea271d6bd2acae9602b5f94a94d006eb1bf
ziontee113 <ziontee113@gmail.com>
2022-12-23 09:51:13 +0700

    Add TailwindCSS

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ function App() {
 
     return (
         <div>
+            <h1 className="text-yellow-500">Hello World</h1>
             <button onClick={() => setCount((count) => count + 1)}>
                 count is {count}
             </button>
